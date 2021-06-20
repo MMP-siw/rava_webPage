81d976034d2660307ce0c9c7e2638e8a2e95c6d2
pietro <quadrinipietro@gmail.com>
2021-06-20 10:56:33 +0200

    style: menu, prodotto e selezionaProdotto

--- siw-spring-progetto-rava/siw-rava/src/main/java/it/uniroma3/siw/spring/rava/controller/mainController.java
+++ siw-spring-progetto-rava/siw-rava/src/main/java/it/uniroma3/siw/spring/rava/controller/mainController.java
@@ -1,11 +1,19 @@
 package it.uniroma3.siw.spring.rava.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.uniroma3.siw.spring.rava.model.Cliente;
+import it.uniroma3.siw.spring.rava.model.Credentials;
+import it.uniroma3.siw.spring.rava.model.Ordine;
+import it.uniroma3.siw.spring.rava.model.Prodotto;
+import it.uniroma3.siw.spring.rava.service.CredentialsService;
 import it.uniroma3.siw.spring.rava.service.ProdottoService;
 
 @Controller
@@ -13,6 +21,8 @@ public class mainController
 {
 	@Autowired
 	private ProdottoService prodottoService;
+	@Autowired
+	private CredentialsService credentialsService;
 	
 	@RequestMapping(value= {"/", "home"}, method=RequestMethod.GET)
 	public String index(Model model)
@@ -25,6 +35,19 @@ public class mainController
 		model.addAttribute("menu", this.prodottoService.tutti());
 		return "menu.html";
 	}
+	
+	@RequestMapping(value="/menu/prodotto/{id2}", method=RequestMethod.GET)
+	public String goProduct(@PathVariable("id2")Long id2, Model model) {
+
+	
+
+		Prodotto prod = this.prodottoService.prodottoPerId(id2);
+
+		model.addAttribute("prodotto", prod);
+		return "prodottoMenu.html";
+	}
+	
+
 
 
 }
